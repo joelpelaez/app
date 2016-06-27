085428ef5ec7834c70ca66587952a34f6381425e
Joel Peláez Jorge <joelpelaez@gmail.com>
2016-06-27 11:23:49 -0500

    Repositorio inicial

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Aplicación</title>
+</head>
+<body>
+    <h1>Página principal</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta laborum odio, aliquid delectus excepturi laudantium odit ratione nulla harum quas voluptas perferendis aperiam velit repellat accusamus dolorem repudiandae ad qui.</p>
+</body>
+</html>
